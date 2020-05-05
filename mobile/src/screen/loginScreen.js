c679f9843b51e016b7483317d1014fe323abd815
Lucas Salvador <lucas.salvador11358@gmail.com>
2020-05-05 11:21:11 -0300

    atualizando componentes e nomes de arquivos

--- mobile/src/screen/loginScreen.js
+++ mobile/src/screen/loginScreen.js
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
 
-function extratoScreen({ navigation }) {
+function loginScreen({ navigation }) {
   return (
     <View style={{ flex: 1, marginRight:25, marginLeft: 25, fontSize: 15, marginTop:40}}>
       <Text style={{ fontWeight:"bold", fontSize: 23, textAlign:"center"}}>Login</Text>
     </View>
   );
 }
-export default extratoScreen;
+export default loginScreen;
