66dd52b795397e0b772a3c7834da4ee456dbfb41
Lucas Salvador <lucas.salvador11358@gmail.com>
2020-05-19 10:18:16 -0300

    adicionando rotas

--- server/src/routes.py
+++ server/src/routes.py
@@ -1,6 +1,23 @@
+from flask import request, json, jsonify
 from src import app
 
 @app.route('/')
-@app.route('/index')
 def index():
-    return "Hello, World!"
+    return "Server Works"
+
+@app.route('/home')
+def get_home():
+    return 'HOMEEE'
+
+@app.route('/conta', methods=['POST'])
+def post_conta():
+    return "salvar conta"
+
+@app.route('/transacao', methods=['POST'])
+def post_transacao():
+    return "salvar transacao"
+
+
+@app.route('/transacoes', methods=['GET'])
+def get_transacoes():
+    return "listar transacao"
