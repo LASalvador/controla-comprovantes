264eefb3efa8170ab66fcf6fdc986e6eef900b99
Monica261 <desenvolvimento.midiasim@gmail.com>
2020-06-21 11:51:48 -0300

    Atualizado

--- mobile/src/screen/escolhaContaScreen.js
+++ mobile/src/screen/escolhaContaScreen.js
@@ -2,6 +2,7 @@ import React , { useState }from 'react';
 import { Button, View, Text,  FlatList } from 'react-native';
 import {StyleSheet} from 'react-native';
 
+
 export default function categoriaScreen(){
   const [categoria] = useState([
     {categoria: 'Pessoa Fisica', key:'1'},
@@ -28,47 +29,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    fontSize:100
   },
   logo:{
     fontWeight:"bold",
     fontSize:50,
     color:"#005795", 
-    marginBottom:40
+    marginBottom:40,
+    marginTop:150
   },
-  inputView:{
-    width:"80%",
-    backgroundColor:"#f2f2f2",
-    borderRadius:25,
-    height:50,
-    marginBottom:20,
-    justifyContent:"center",
-    padding:20
-  },
-  inputText:{
-    height:50,
-    color:"black"
-  },
-  loginBtn:{
-    width:"80%",
-    backgroundColor:"#005796",
-    borderRadius:25,
-    height:50,
-    alignItems:"center",
-    justifyContent:"center",
-    marginTop:40,
-    marginBottom:10
-  },
-  fundo:{
-    width:"80%",
-    color: "#005796",
-    backgroundColor:"#f2f2f2",
-    borderRadius:25,
-    height:50,
-    marginBottom:20,
-    justifyContent:"center",
-    padding:10
-  },
-  loginText:{
-    color:"white"
-  }
+  
 });
