17c911a560ac46a29dc6eb86efdc07e7edd6ed53
Lucas Salvador <lucas.salvador11358@gmail.com>
2020-06-25 21:33:45 -0300

    adicionando conexão com o banco

--- mobile/src/screen/escolhaContaScreen.js
+++ mobile/src/screen/escolhaContaScreen.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react'
-import { View, Text,  FlatList, Alert } from 'react-native';
+import { View, Text,  FlatList, AsyncStorage } from 'react-native';
 import {StyleSheet} from 'react-native';
 import BotaoFluante from '../components/BotaoFlutuante';
 import Constants from 'expo-constants';
+import api from '../services/api';
 
 export default class escolhaContaScreen extends Component {
   
   state = {
-    contas_list: [{conta: 'Pessoa Fisica', key:'1'},{conta: 'Pessoa Juridica', key:'2'}],
+    contas_list: [],
   }
 
   handleClick = () => {
     this.props.navigation.navigate('Nova Conta')
   }
 
-  componentDidMount () {
-    // this.setState({contas_list: []})
+  async componentDidMount () {
+    const user_id = await AsyncStorage.getItem("userId");
+    const response = await api.get(`conta/${user_id}`);
+    const conta_list = response.data.contas_usuario.map(item => {
+      return {
+        key: item.conta_id,
+        conta:item.perfil
+      }
+    })
+
+    this.setState({contas_list: conta_list});
+
   }
 
   handleChange = () => {
-    Alert.alert('mudaaar')
+    
   }
 
   render() {
@@ -57,6 +68,7 @@ const styles = StyleSheet.create({
     marginTop: 10,
     padding: 20,
     marginHorizontal: 16,
+    borderRadius: 25,
   },
   logo:{
     fontWeight:"bold",
