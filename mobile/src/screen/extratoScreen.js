0c5f58e5fe69971989ef90b6bdc6cba11e417b37
Monica261 <desenvolvimento.midiasim@gmail.com>
2020-06-25 23:06:35 -0300

    AtualizaÃÃo da tela Extrato

--- mobile/src/screen/extratoScreen.js
+++ mobile/src/screen/extratoScreen.js
@@ -1,87 +1,50 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, TextInput, Button, View } from 'react-native';
 import { FontAwesome5 } from '@expo/vector-icons'; 
+import { ListItem, FlatList } from 'react-native-elements';
+import Constants from 'expo-constants';
+
+
+
+
+const list = [
+{
+  name: 'Alimentação',
+  avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+  tipo: 'saida'
+},
+{
+  name: 'Transporte',
+  avatar_url: '  faces/twitter/adhamdannaway/128.jpg',
+  tipo: 'saida',
+},
+
+]
+
+
 
 export default class extratoScreen extends Component {
 
-  state={
-    myKey: null
-  }
-  constructor(props) {
-    super(props);
-  }
+ render() {
+  return (
+    <View>
+   <Text style={{fontSize: 50,color: "#005795",textAlign: 'center',fontWeight:"bold",marginBottom:40,marginTop: Constants.statusBarHeight,}}>Extrato</Text>
+    {
+      list.map((l, i) => (
+        <ListItem
+        key={i}
+        leftAvatar={{ source: { uri: l.avatar_url } }}
+        title={l.name}
+        subtitle={l.tipo}
+        bottomDivider
+        />
 
-  setValue(value) {
-      this.setState({myKey:value})
-  }
+        ))
+    }
+
+    </View>
+  )
+}}
 
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.logo}>
-         <FontAwesome5 name="coins" size={25} color="black"/>
-          Conta
-        </Text>
-
-        <Text style={styles.saldo}>
-          Saldo disponivel
-        </Text>
-        <TextInput
-          style={styles.formInput}
-          placeholder="Ex: 2.000"
-          value={this.state.myKey}
-          onChangeText={(value) => this.setValue(value)}
-          />      
-
-         {/*<Text style={styles.instructions}>
-          Saldo = {this.state.myKey}
-        </Text>*/}
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    padding: 30,
-    flex: 1,
-    alignItems: 'center',
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-    marginTop: -90,
-  },
-  logo:{
-    fontWeight:"bold",
-    fontSize:50,
-    color:"#005795", 
-    marginBottom:40,
-    textAlign: 'center',
-  },
-  formInput: {
-    paddingLeft: 55,
-    paddingRight: 55,
-    height: 50,
-    borderWidth: 1,
-    borderColor: "#555555",
-    borderRadius:25,
-    color: '#000',
-    marginRight: 130,
-  },
-  saldo:{
-    color: '#d6d6d6',
-    marginBottom: 10,
-    textAlign: 'left',
-    marginRight: 130,
-    fontSize: 20,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-    marginTop: 15,
-    fontSize: 25,
-    fontWeight: 'bold',
-  },
-});
 
